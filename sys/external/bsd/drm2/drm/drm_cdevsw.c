e8b3916747c77ff4e3750cace1987582f172c8fa
Taylor R Campbell <riastradh@NetBSD.org>
2021-08-19 21:46:14 +0000

    drm: Try setting D_MPSAFE -- maybe it'll stop the spinouts.

--- sys/external/bsd/drm2/drm/drm_cdevsw.c
+++ sys/external/bsd/drm2/drm/drm_cdevsw.c
@@ -97,8 +97,7 @@ const struct cdevsw drm_cdevsw = {
 	.d_kqfilter = nokqfilter,
 	.d_discard = nodiscard,
 	/* XXX was D_TTY | D_NEGOFFSAFE */
-	/* XXX Add D_MPSAFE some day... */
-	.d_flag = D_NEGOFFSAFE,
+	.d_flag = D_MPSAFE|D_NEGOFFSAFE,
 };
 
 const struct fileops drm_fileops = {
